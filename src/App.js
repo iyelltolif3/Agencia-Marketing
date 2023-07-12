a735a4f55a44bcaca733ce4fe0b392d0cc68a770
iyelltolif3 <103101053+iyelltolif3@users.noreply.github.com>
2023-07-12 11:31:48 -0400

    Add: home.jsx and Errore404.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Error404 from "containers/errors/Error404";
+import Home from "containers/pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="text-gray-500 underline">
-      Holamundo
-    </div>
-  );
+  <Router>
+    <Routes>
+      <Route path="*" element={<Error404/>} />
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </Router>;
 }
 
 export default App;
